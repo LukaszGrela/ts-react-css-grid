6ec9b96aec569b8f9389586cef99677294236a99
Lukasz Grela <lukasz.grela@gmail.com>
2019-09-21 00:43:22 +0100

    Create some click handlers Create IProps

--- src/components/Toolbox/Toolbox.tsx
+++ src/components/Toolbox/Toolbox.tsx
@@ -2,13 +2,51 @@ import React from 'react';
 
 import './styles/index.scss';
 
-const Toolbox: React.FC = (): JSX.Element => {
+export const ADD = 'ADD';
+export const REMOVE = 'REMOVE';
+
+export type TAction = typeof ADD | typeof REMOVE;
+export interface IProps {
+  modifyColumn: (action: TAction) => void;
+  modifyRow: (action: TAction) => void;
+
+  columns: number;
+  rows: number;
+}
+
+const Toolbox: React.FC<IProps> = ({
+  modifyColumn,
+  modifyRow,
+  columns,
+  rows,
+}: IProps): JSX.Element => {
+  const addColumn = (): void => modifyColumn(ADD);
+  const removeColumn = (): void => modifyColumn(REMOVE);
+  const addRow = (): void => modifyRow(ADD);
+  const removeRow = (): void => modifyRow(REMOVE);
   return (
     <div className="Toolbox">
-      <button className="Toolbox_button">Add Column</button>
-      <button className="Toolbox_button">Delete Column</button>
-      <button className="Toolbox_button">Add Row</button>
-      <button className="Toolbox_button">Delete Row</button>
+      <span>
+        {columns}, {rows}
+      </span>
+      <button className="Toolbox_button" disabled={false} onClick={addColumn}>
+        Add Column
+      </button>
+      <button
+        className="Toolbox_button"
+        disabled={columns <= 1}
+        onClick={removeColumn}>
+        Delete Column
+      </button>
+      <button className="Toolbox_button" disabled={false} onClick={addRow}>
+        Add Row
+      </button>
+      <button
+        className="Toolbox_button"
+        disabled={rows <= 1}
+        onClick={removeRow}>
+        Delete Row
+      </button>
     </div>
   );
 };
