022386ce7eff12f6bc829d9a623e9655bfe4a789
Lukasz Grela <lukasz.grela@gmail.com>
2019-09-21 00:43:37 +0100

    Wire state/dispatch

--- src/components/Toolbox/ToolboxConnected.ts
+++ src/components/Toolbox/ToolboxConnected.ts
@@ -1,4 +1,44 @@
 import { connect } from 'react-redux';
-import Toolbox from './Toolbox';
+import Toolbox, {
+  IProps as IToolboxProps,
+  TAction,
+  ADD,
+  REMOVE,
+} from './Toolbox';
+import { AppState } from '../../store';
+import { Dispatch } from 'redux';
+import {
+  addColumn,
+  removeColumn,
+  addRow,
+  removeRow,
+} from '../../store/Grid/actions';
 
-export default connect()(Toolbox);
+type TStateProps = Pick<IToolboxProps, 'columns' | 'rows'>;
+const mapStateToProps = (state: AppState): TStateProps => ({
+  columns: state.grid.columns,
+  rows: state.grid.rows,
+});
+
+type TDispatchProps = Pick<IToolboxProps, 'modifyColumn' | 'modifyRow'>;
+const mapDispatchToProps = (dispatch: Dispatch): TDispatchProps => ({
+  modifyColumn: (action: TAction): void => {
+    if (action === ADD) {
+      dispatch(addColumn());
+    } else if (action === REMOVE) {
+      dispatch(removeColumn());
+    }
+  },
+  modifyRow: (action: TAction): void => {
+    if (action === ADD) {
+      dispatch(addRow());
+    } else if (action === REMOVE) {
+      dispatch(removeRow());
+    }
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Toolbox);
