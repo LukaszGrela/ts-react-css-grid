2737ccd1d6cd4f09bbde365a6d510ee586864dbc
Lukasz Grela <lukasz.grela@gmail.com>
2019-09-21 00:50:05 +0100

    Handle IModifyGrid actions

--- src/store/Grid/reducer.ts
+++ src/store/Grid/reducer.ts
@@ -1,4 +1,11 @@
-import { IModifyGridAction, IGridState } from './types';
+import {
+  IModifyGridAction,
+  IGridState,
+  ADD_COLUMN,
+  REMOVE_COLUMN,
+  ADD_ROW,
+  REMOVE_ROW,
+} from './types';
 
 const initialState: IGridState = {
   columns: 1,
@@ -9,7 +16,19 @@ const reducer = (
   state = initialState,
   action: IModifyGridAction
 ): IGridState => {
-  return state;
+  switch (action.type) {
+    case ADD_COLUMN:
+      return { ...state, columns: state.columns + 1 };
+    case REMOVE_COLUMN:
+      return { ...state, columns: Math.max(state.columns - 1, 0) };
+    case ADD_ROW:
+      return { ...state, rows: state.rows + 1 };
+    case REMOVE_ROW:
+      return { ...state, rows: Math.max(state.rows - 1, 0) };
+
+    default:
+      return state;
+  }
 };
 
 export default reducer;
