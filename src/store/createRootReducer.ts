130f1a9eaf23f076f20b2fb094cf71e0b196ed68
Lukasz Grela <lukasz.grela@gmail.com>
2019-09-21 02:20:11 +0100

    add gridItems reducer to store

--- src/store/createRootReducer.ts
+++ src/store/createRootReducer.ts
@@ -1,16 +1,20 @@
 import { combineReducers, Reducer, AnyAction } from 'redux';
 import { IGridState } from './Grid/types';
 import grid from './Grid/reducer';
+import gridItems from './GridItems/reducer';
+import { IGridItemsState } from './GridItems/types';
 
 export type TRootReducer = Reducer<
   {
     grid: IGridState;
+    gridItems: IGridItemsState;
   },
   AnyAction
 >;
 const createRootReducer = (): TRootReducer =>
   combineReducers({
     grid,
+    gridItems,
   });
 
 export default createRootReducer;
