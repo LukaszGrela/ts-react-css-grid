ba4fa552b4a66164af31af82ee01a1a5afe649e0
Lukasz Grela <lukasz.grela@gmail.com>
2019-09-21 14:18:55 +0100

    add NumberSpinner for row/column gap

--- src/components/Toolbox/Toolbox.tsx
+++ src/components/Toolbox/Toolbox.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './styles/index.scss';
+import NumberSpinner from '../NumberSpinner/NumberSpinner';
 
 export const ADD = 'ADD';
 export const REMOVE = 'REMOVE';
@@ -30,42 +31,68 @@ const Toolbox: React.FC<IProps> = ({
   const removeColumn = (): void => modifyColumn(REMOVE);
   const addRow = (): void => modifyRow(ADD);
   const removeRow = (): void => modifyRow(REMOVE);
+
+  const changeRowGap = (newValue: number): void => {
+    console.log('changeRowGap', newValue);
+  };
+
+  const changeColumnGap = (newValue: number): void => {
+    console.log('changeRowGap', newValue);
+  };
+
   return (
     <div className="Toolbox">
-      <span>
-        {columns}, {rows}
-      </span>
-      <button className="Toolbox_button" disabled={false} onClick={addColumn}>
-        Add Column
-      </button>
-      <button
-        className="Toolbox_button"
-        disabled={columns <= 1}
-        onClick={removeColumn}>
-        Delete Column
-      </button>
-      <button className="Toolbox_button" disabled={false} onClick={addRow}>
-        Add Row
-      </button>
-      <button
-        className="Toolbox_button"
-        disabled={rows <= 1}
-        onClick={removeRow}>
-        Delete Row
-      </button>
-      <hr className="Toolbox_spacer" />
-      <button
-        className="Toolbox_button"
-        disabled={columns * rows <= gridItemsLength && gridItemsLength !== 0}
-        onClick={addGridItem}>
-        Add grid item
-      </button>
-      <button
-        className="Toolbox_button"
-        disabled={gridItemsLength === 0}
-        onClick={clearGridItems}>
-        Remove all items
-      </button>
+      <div className="Toolbox_wrapper">
+        <span>
+          {columns}, {rows}
+        </span>
+        <button className="Toolbox_button" disabled={false} onClick={addColumn}>
+          Add Column
+        </button>
+        <button
+          className="Toolbox_button"
+          disabled={columns <= 1}
+          onClick={removeColumn}>
+          Delete Column
+        </button>
+        <button className="Toolbox_button" disabled={false} onClick={addRow}>
+          Add Row
+        </button>
+        <button
+          className="Toolbox_button"
+          disabled={rows <= 1}
+          onClick={removeRow}>
+          Delete Row
+        </button>
+        <hr className="Toolbox_spacer" />
+        <button
+          className="Toolbox_button"
+          disabled={columns * rows <= gridItemsLength && gridItemsLength !== 0}
+          onClick={addGridItem}>
+          Add grid item
+        </button>
+        <button
+          className="Toolbox_button"
+          disabled={gridItemsLength === 0}
+          onClick={clearGridItems}>
+          Remove all items
+        </button>
+        <hr className="Toolbox_spacer" />
+        <span className="Toolbox_label">Set row gap</span>
+        <NumberSpinner
+          disableNext={false}
+          disablePrev={true}
+          value={1}
+          onChange={changeRowGap}
+        />
+        <span className="Toolbox_label">Set column gap</span>
+        <NumberSpinner
+          disableNext={false}
+          disablePrev={true}
+          value={0}
+          onChange={changeColumnGap}
+        />
+      </div>
     </div>
   );
 };
