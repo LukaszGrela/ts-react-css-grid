7efbe23f9e969ba98e5b7bf4b68aee151c1c81b8
Lukasz Grela <lukasz.grela@gmail.com>
2019-09-22 12:17:27 +0100

    throttle drag update event

--- src/components/GridView/GridViewConnected.ts
+++ src/components/GridView/GridViewConnected.ts
@@ -6,9 +6,11 @@ import {
   dragItemStart,
   dragItemStop,
   repositionGridItem,
+  dragItemUpdate,
 } from '../../store/GridItems/actions';
 
 import { throttle } from 'lodash';
+import { ICoords2D } from '../../utils/2d-array-utils';
 
 const clamp = (val: number, min: number, max: number) =>
   Math.max(min, Math.min(val, max));
@@ -30,7 +32,30 @@ type TDispatchProps = Pick<
   IProps,
   'onStartDrag' | 'onUpdateDrag' | 'onStopDrag'
 >;
+const convertToGridCoords = (
+  x: number,
+  y: number,
+  columns: number,
+  rows: number,
+  columnSize: number,
+  rowSize: number
+): ICoords2D => {
+  const halfPadding = 8;
+
+  const gridX = clamp(
+    Math.round((x - halfPadding + columnSize / 2) / columnSize),
+    1,
+    columns
+  );
+  const gridY = clamp(
+    Math.round((y - halfPadding + rowSize / 2) / rowSize),
+    1,
+    rows
+  );
+  return { x: gridX, y: gridY };
+};
 const onUpdateDrag = (
+  dispatch: Dispatch,
   id: string,
   x: number,
   y: number,
@@ -38,36 +63,37 @@ const onUpdateDrag = (
   rows: number,
   target: HTMLElement
 ): void => {
-  const halfPadding = 8;
   const columnSize = target.offsetParent
     ? target.offsetParent.clientWidth / columns
     : 0;
   const rowSize = target.offsetParent
     ? target.offsetParent.clientHeight / rows
     : 0;
-  console.log(
-    'UPDATE DRAG',
-    id,
-    clamp(
-      Math.round((x - halfPadding + columnSize / 2) / columnSize),
-      1,
-      columns
-    ),
-    clamp(Math.round((y - halfPadding + rowSize / 2) / rowSize), 1, rows)
-  );
+  const coords = convertToGridCoords(x, y, columns, rows, columnSize, rowSize);
+  console.log('UPDATE DRAG', id, coords.x, coords.y);
   const marker = document.getElementById('marker');
   if (marker) {
     marker.style.top = `${y}px`;
     marker.style.left = `${x}px`;
   }
+  dispatch(dragItemUpdate(id, coords.x, coords.y));
 };
-const throttledOnUpdateDrag = throttle(onUpdateDrag, 333);
+const throttledOnUpdateDrag = throttle(onUpdateDrag, 333, { trailing: false });
 const mapDispatchToProps = (dispatch: Dispatch): TDispatchProps => ({
   onStartDrag: (id: string): void => {
     console.log('START DRAG', id);
     dispatch(dragItemStart(id));
   },
-  onUpdateDrag: throttledOnUpdateDrag,
+  onUpdateDrag: (
+    id: string,
+    x: number,
+    y: number,
+    columns: number,
+    rows: number,
+    target: HTMLElement
+  ): void => {
+    throttledOnUpdateDrag(dispatch, id, x, y, columns, rows, target);
+  },
   onStopDrag: (
     id: string,
     x?: number,
@@ -77,28 +103,28 @@ const mapDispatchToProps = (dispatch: Dispatch): TDispatchProps => ({
     target?: HTMLElement
   ): void => {
     console.log('STOP DRAG', id);
-    if (target && x !== undefined && y != undefined) {
+    if (
+      target &&
+      x !== undefined &&
+      y != undefined &&
+      columns !== undefined &&
+      rows != undefined
+    ) {
       const columnSize = target.offsetParent
-        ? target.offsetParent.clientWidth / (columns || 0)
+        ? target.offsetParent.clientWidth / columns
         : 0;
       const rowSize = target.offsetParent
-        ? target.offsetParent.clientHeight / (rows || 0)
+        ? target.offsetParent.clientHeight / rows
         : 0;
-      console.log(
-        'UPDATE DRAG',
-        id,
+      const coords = convertToGridCoords(
         x,
         y,
-        clamp(
-          Math.round((x + 16 + columnSize / 2) / columnSize),
-          1,
-          columns || 0
-        ),
-        clamp(Math.round((y + 16 + rowSize / 2) / rowSize), 1, rows || 0),
+        columns,
+        rows,
         columnSize,
         rowSize
       );
-      dispatch(repositionGridItem(id, x || -1, y || -1));
+      dispatch(repositionGridItem(id, coords.x, coords.y));
     }
 
     dispatch(dragItemStop(id));
