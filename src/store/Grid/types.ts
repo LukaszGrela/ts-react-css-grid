d80c609d2b2179fc0af649ce9b65659d54d0c9e0
Lukasz Grela <lukasz.grela@gmail.com>
2020-12-07 09:14:55 +0000

    union grid actions

--- src/store/Grid/types.ts
+++ src/store/Grid/types.ts
@@ -1,12 +1,12 @@
-import { AnyAction } from 'redux';
+import { AnyAction } from "redux";
 
-export const ADD_COLUMN = 'grid/column/ADD';
-export const REMOVE_COLUMN = 'grid/column/REMOVE';
-export const ADD_ROW = 'grid/row/ADD';
-export const REMOVE_ROW = 'grid/row/REMOVE';
+export const ADD_COLUMN = "grid/column/ADD";
+export const REMOVE_COLUMN = "grid/column/REMOVE";
+export const ADD_ROW = "grid/row/ADD";
+export const REMOVE_ROW = "grid/row/REMOVE";
 
-export const SET_ROW_GAP = 'grid/gap/row/SET';
-export const SET_COLUMN_GAP = 'grid/column/row/SET';
+export const SET_ROW_GAP = "grid/gap/row/SET";
+export const SET_COLUMN_GAP = "grid/column/row/SET";
 
 export interface IModifyGridAction extends AnyAction {
   type:
@@ -21,7 +21,7 @@ export interface IChangeGridGap extends AnyAction {
   value: number;
 }
 
-export type TGridActions = IModifyGridAction & IChangeGridGap;
+export type TGridActions = IModifyGridAction | IChangeGridGap;
 
 export interface ICSSGridStyle {
   gridTemplateColumns?: string;
